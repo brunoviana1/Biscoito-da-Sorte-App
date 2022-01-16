ee9c03d85797124e3060152304e5f1f88d22680c
Bruno Viana <bruno_cotta2010@hotmail.com>
2022-01-16 11:58:15 -0300

    Aplicativo Biscoito da Sorte

---
+++ Biscoito/App.js
@@ -0,0 +1,139 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+function App(){
+
+  const[img, setImg] = useState(require('./src/biscoito.png'));
+  const[textoFrase, setTextoFrase] = useState('teste frase');
+
+  let frases = [
+    'A vida trará coisas boas se tiveres paciência.',
+    'Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você.',
+    'Não compense na ira o que lhe falta na razão.',
+    'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+    'A maior de todas as torres começa no solo.',
+    'Não há que ser forte. Há que ser flexível.',
+    'Gente todo dia arruma os cabelos, por que não o coração?',
+    'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
+    'A juventude não é uma época da vida, é um estado de espírito.',
+    'Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.',
+    'Dê toda a atenção para a formação dos teus filhos, sobretudo por exemplos de tua própria vida.',
+    'Siga os bons e aprenda com eles.',
+    'Não importa o tamanho da montanha, ela não pode tapar o sol.',
+    'O bom-senso vai mais longe do que muito conhecimento.',
+    'Quem quer colher rosas deve suportar os espinhos.',
+    'São os nossos amigos que nos ensinam as mais valiosas lições.',
+    'Uma iniciativa mal-sucedida não significa o final de tudo. Sempre existe uma nova oportunidade.',
+    'Aquele que se importa com o sentimento dos outros, não é um tolo.',
+    'A adversidade é um espelho que reflete o verdadeiro eu.',
+    'Lamentar aquilo que não temos é desperdiçar aquilo que já possuímos.',
+    'Uma bela flor é incompleta sem suas folhas.',
+    'Sem o fogo do entusiasmo, não há o calor da vitória.',
+    'Não há melhor negócio que a vida. A gente há obtém a troco de nada.',
+    'O riso é a menor distância entre duas pessoas.',
+    'Você é jovem apenas uma vez. Depois precisa inventar outra desculpa.',
+    'É mais fácil conseguir o perdão do que a permissão.',
+    'Os defeitos são mais fortes quando o amor é fraco.',
+    'Amizade e Amor são coisas que podem virar uma só num piscar de olhos.',
+    'Surpreender e ser surpreendido é o segredo do amor.',
+    'Faça pequenas coisas agora e maiores coisas lhe serão confiadas cada dia.',
+    'Todo mundo é capaz de denominar uma dor, exceto quem a sente.',
+    'A paciência na adversidade é o sinal de um coração sensível.',
+    'A sorte favorece a mente bem preparada.',
+  ];
+
+  function quebraBiscoito(){
+    let numeroAleatorio = Math.floor(Math.random() * frases.length )
+    setTextoFrase(' " ' + frases[numeroAleatorio] + ' " ');
+    setImg(require('./src/biscoitoAberto.png'))
+  }
+
+  function reiniciarBiscoito(){
+    setImg(require('./src/biscoito.png'));
+    setTextoFrase('');
+  }
+
+
+  return(
+      <View style={styles.container} >
+        
+        <Image
+        source={img}
+        style={styles.img}
+        />
+
+
+        <Text style={styles.textofrase}> {textoFrase} </Text>
+
+        <TouchableOpacity style={styles.botaoQuebrar} onPress={ quebraBiscoito }  >
+          <View  style={styles.btnArea} >
+            <Text style={styles.btntexto}  > Quebrar Biscoito </Text>
+          </View>
+        </TouchableOpacity>
+
+        
+        <TouchableOpacity style={[styles.botaoQuebrar, styles. botaoReiniciar ]} onPress={ reiniciarBiscoito}  >
+          <View  style={styles.btnArea} >
+            <Text style={[styles.btntexto, styles.btnTextReiniciar]}  > Reiniciar Biscoito </Text>
+          </View>
+        </TouchableOpacity>
+
+      </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',     
+    alignItems: 'center'
+  },
+
+  img:{
+    width: 230,
+    height: 230,
+  },
+
+  textofrase:{
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+
+  botaoQuebrar:{
+    width: 230,
+    height: 50,
+    borderColor: '#dd7b22',
+    borderWidth: 2,
+    borderRadius: 25,   
+  },
+
+  btnArea:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  btntexto:{
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#dd7b22'
+  },
+
+  botaoReiniciar:{
+    marginTop: 15,
+    borderColor: '#121212'
+  },
+
+  btnTextReiniciar:{
+    color: '#121212'
+  }
+
+
+
+})
+
+export default App;
